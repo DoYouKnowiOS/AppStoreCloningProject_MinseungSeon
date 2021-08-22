f66807e7660d65850f409b3b4b3cd0058b9051fa
MinseungSeon <seonminseung@naver.com>
2021-08-22 14:10:29 +0900

    first commit

---
+++ AppStoreCloning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStoreCloning
+//
+//  Created by 선민승 on 2021/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# AppStoreCloningProject_MinseungSeon
